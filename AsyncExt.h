ebcbc018f45999922715bda39b0c19c923a21103
chenhong <mail2hong@gmail.com>
2015-10-25 22:58:59 +0800

    sample code

--- AsyncExt.h
+++ AsyncExt.h
@@ -5,6 +5,22 @@
 
 typedef BOOL (^work_ret_bool_stop)(void);
 
+/*
+	// 	example : try 3 time to wait for coming data
+    [AsyncExt dispath_iterate:3 interval_seconds:1 work:^BOOL{
+    
+        if( if_data_ready() )
+        {
+ 			// do handling
+            return true; // to stop
+        }
+        else
+        {
+            return false;
+        }
+    }];
+
+*/
 +(void) dispath_iterate:(size_t)a_iterations  interval_seconds:(int64_t) interval_seconds work:(work_ret_bool_stop )work;
 
 
